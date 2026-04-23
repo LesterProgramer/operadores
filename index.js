6752861789daf7d75a8ee3f3f961fc8cd4882a05
Lester <programmer@lester.com.mx>
2026-04-23 17:44:33 -0600

    ADD funcion de resta

---
+++ index.js
@@ -0,0 +1,16 @@
+const suma  = () => {
+    const a = 2
+    const b = 3
+
+    let sumaV = a + b;
+
+    console.log(sumaV)
+}
+
+const resta = () => {
+    const a = 4;
+    const b = 5; 
+
+    let restaV = a - b;
+    console.log(restaV)
+}
